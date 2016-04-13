1f2caf039edca6aeadccb11fd83cf3909c2abd6f
Enes Çakır <enes@cakir.web.tr>
2016-04-13 22:01:58 +0300

    Webservice is changed

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -18,7 +18,7 @@ class ApiController extends Controller
         $request['ip_adress'] = $request->ip();
         $visitor = Visitor::firstOrCreate($request->only(['device_id','ip_adress']));
         $request['visitor_id'] = $visitor->id;
-        $visit = Visit::firstOrCreate($request->only(['visitor_id', 'exam_id']));
+        $visit = Visit::firstOrCreate($request->only(['visitor_id', 'exam_id','platform']));
         return 'Success';
     }
 
