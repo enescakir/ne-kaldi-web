337a6a2f183246b3c4a59a5d79b02e4953cf2304
Enes Çakır <enes@cakir.web.tr>
2016-08-14 03:05:27 +0300

    Soft delete added

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use Illuminate\Http\Request;
+use Session;
 
 class AuthController extends Controller
 {
