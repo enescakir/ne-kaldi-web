1b8f7f5e9369587eb59c0bd27029ade7a5d50840
Enes Çakır <enes@cakir.web.tr>
2016-10-14 09:45:52 +0300

    cron added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,7 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            Log::info('Cron job called');
+        })->everyThirtyMinutes();;
     }
 }
