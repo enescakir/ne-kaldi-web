eaae00924d9a061e9d96e3cc2e119b71c7f5eafa
Enes Çakır <enes@cakir.web.tr>
2018-12-20 16:37:49 +0300

    Throttle update

--- app/Exam.php
+++ app/Exam.php
@@ -11,7 +11,10 @@ class Exam extends Model
 {
     use SoftDeletes;
     protected $dates = ['deleted_at', 'created_at', 'updated_at', 'date', 'start', 'end'];
-
+    protected $hidden = [
+        'created_at', 'updated_at', 'deleted_at', 'created_by', 'updated_by', 'deleted_by'
+    ];
+    
     public static function boot() {
         parent::boot();
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,28 +12,24 @@
 */
 
 
-
-
-
-
 Route::group(['prefix' => 'api'], function () {
-    Route::group(['prefix' => 'v1'], function () {
-        Route::get('/exams', array('as' => 'api.v1.exams', 'uses' => 'APIv1Controller@exams'));
-        Route::post('/subscribe', array('as' => 'api.v1.subscribe', 'uses' => 'APIv1Controller@subscribe'));
-        Route::post('/unsubscribe', array('as' => 'api.v1.unsubscribe', 'uses' => 'APIv1Controller@unsubscribe'));
-        Route::post('/visit', array('as' => 'api.v1.visit', 'uses' => 'APIv1Controller@visit'));
-        Route::post('/favorite', array('as' => 'api.v1.favorite', 'uses' => 'APIv1Controller@favorite'));
+    Route::group(['prefix' => 'v1', 'middleware' => 'throttle:60,1'], function () {
+        Route::get('/exams', ['as' => 'api.v1.exams', 'uses' => 'APIv1Controller@exams']);
+        Route::post('/subscribe', ['as' => 'api.v1.subscribe', 'uses' => 'APIv1Controller@subscribe']);
+        Route::post('/unsubscribe', ['as' => 'api.v1.unsubscribe', 'uses' => 'APIv1Controller@unsubscribe']);
+        Route::post('/visit', ['as' => 'api.v1.visit', 'uses' => 'APIv1Controller@visit']);
+        Route::post('/favorite', ['as' => 'api.v1.favorite', 'uses' => 'APIv1Controller@favorite']);
     });
 
-    Route::group(['prefix' => 'v2'], function () {
-        Route::get('/exams', array('as' => 'api.v2.exams', 'uses' => 'APIv2Controller@exams'));
-        Route::post('/subscribe', array('as' => 'api.v2.subscribe', 'uses' => 'APIv2Controller@subscribe'));
-        Route::post('/exam', array('as' => 'api.v2.exam', 'uses' => 'APIv2Controller@exam'));
-        Route::post('/visitor/action', array('as' => 'api.v2.visitor.action', 'uses' => 'APIv2Controller@visitorAction'));
-        Route::post('/message', array('as' => 'api.v2.message', 'uses' => 'APIv2Controller@message'));
-        Route::post('/unsubscribe', array('as' => 'api.v2.unsubscribe', 'uses' => 'APIv2Controller@unsubscribe'));
-        Route::post('/visit', array('as' => 'api.v2.visit', 'uses' => 'APIv2Controller@visit'));
-        Route::post('/favorite', array('as' => 'api.v2.favorite', 'uses' => 'APIv2Controller@favorite'));
+    Route::group(['prefix' => 'v2', 'middleware' => 'throttle:60,1'], function () {
+        Route::get('/exams', ['as' => 'api.v2.exams', 'uses' => 'APIv2Controller@exams']);
+        Route::post('/subscribe', ['as' => 'api.v2.subscribe', 'uses' => 'APIv2Controller@subscribe']);
+        Route::post('/exam', ['as' => 'api.v2.exam', 'uses' => 'APIv2Controller@exam']);
+        Route::post('/visitor/action', ['as' => 'api.v2.visitor.action', 'uses' => 'APIv2Controller@visitorAction']);
+        Route::post('/message', ['as' => 'api.v2.message', 'uses' => 'APIv2Controller@message']);
+        Route::post('/unsubscribe', ['as' => 'api.v2.unsubscribe', 'uses' => 'APIv2Controller@unsubscribe']);
+        Route::post('/visit', ['as' => 'api.v2.visit', 'uses' => 'APIv2Controller@visit']);
+        Route::post('/favorite', ['as' => 'api.v2.favorite', 'uses' => 'APIv2Controller@favorite']);
     });
 
 });
@@ -53,7 +49,7 @@ Route::get('/', 'FrontController@home')->name('home');
 
 Route::auth();
 Route::get('/exams/customs', 'ExamController@customs')->name('exams.customs.index');
-Route::post('/exams/{exam}/activate', array('as' => 'exams.activate', 'uses' => 'ExamController@activate'));
+Route::post('/exams/{exam}/activate', ['as' => 'exams.activate', 'uses' => 'ExamController@activate']);
 Route::resource('exams', 'ExamController');
 
 Route::resource('visits', 'VisitController');
@@ -66,13 +62,13 @@ Route::get('notification/test', 'NotificationController@test')->name('notificati
 Route::resource('notification', 'NotificationController');
 Route::post('notification/{notification}/send', 'NotificationController@send')->name('notification.send');
 
-Route::group([ 'prefix' => 'logs'], function() {
-    Route::get('/', [ 'as'    => 'log-viewer::dashboard',  'uses'  => 'LogController@index',]);
-    Route::get('/lists', [ 'as'    => 'log-viewer::logs.list',  'uses'  => 'LogController@listLogs',]);
-    Route::delete('delete', ['as'    => 'log-viewer::logs.delete', 'uses'  => 'LogController@delete',]);
-    Route::group([ 'prefix'    => '{date}',], function() {
-        Route::get('/', ['as'    => 'log-viewer::logs.show', 'uses'  => 'LogController@show',]);
-        Route::get('download', ['as'    => 'log-viewer::logs.download', 'uses'  => 'LogController@download',]);
-        Route::get('{level}', ['as'    => 'log-viewer::logs.filter', 'uses'  => 'LogController@showByLevel',]);
+Route::group(['prefix' => 'logs'], function () {
+    Route::get('/', ['as' => 'log-viewer::dashboard', 'uses' => 'LogController@index',]);
+    Route::get('/lists', ['as' => 'log-viewer::logs.list', 'uses' => 'LogController@listLogs',]);
+    Route::delete('delete', ['as' => 'log-viewer::logs.delete', 'uses' => 'LogController@delete',]);
+    Route::group(['prefix' => '{date}',], function () {
+        Route::get('/', ['as' => 'log-viewer::logs.show', 'uses' => 'LogController@show',]);
+        Route::get('download', ['as' => 'log-viewer::logs.download', 'uses' => 'LogController@download',]);
+        Route::get('{level}', ['as' => 'log-viewer::logs.filter', 'uses' => 'LogController@showByLevel',]);
     });
 });
